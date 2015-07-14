64c721a5bf41ba69a692b74ed1290cf142086016
gcrevell <gcrevell@mtu.edu>
2015-07-14 19:54:54 -0400

    Initial Commit

---
+++ Icon Exporter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Icon Exporter
+//
+//  Created by Voltage on 7/14/15.
+//  Copyright © 2015 Gabriel Revells. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Icon Exporter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Icon Exporter
+//
+//  Created by Voltage on 7/14/15.
+//  Copyright © 2015 Gabriel Revells. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
